a6661ef4039fafe72d9c545c27ae3e717b89c984
Andrei Neustroev <andreineustroev02@gmail.com>
2022-04-17 14:44:10 +0900

    deleted vote subscriber, added vote transaction

--- src/resolvers/vote.resolver.ts
+++ src/resolvers/vote.resolver.ts
@@ -14,6 +14,7 @@ import { IsAuth } from "../utils/middleware/isAuth";
 import { MyContext } from "../types";
 import { AppDataSource } from "../data-source";
 import { Post } from "../entity/post";
+import { Comment } from "../entity/comment";
 
 @ObjectType()
 class VoteResponse {
@@ -43,44 +44,23 @@ export class VoteResolver {
     if (oldVote?.value === realValue) {
       return {
         voted: false,
-        message: "already voted"
+        message: "invalid vote"
       };
     }
 
-    if (alreadyVoted) {
-
-      AppDataSource.transaction(async (entityManager) => {
-        try {
-          await entityManager.delete(PostVote, {
-            userId,
-            postId,
-          })
-          const post = await entityManager.findOne(Post, {
-            where: { id: postId },
-            relations: { author: true }
-          });
-
-
-          post.author.rating -= realValue;
-          post.rating -= realValue;
-          post.author.save();
-          post.save();
-
-          return { voted: false, message: "removed vote" };
-        } catch (error) {
-          return { voted: false, message: "database error" };
-
-        }
-      })
-      // await PostVote.remove(oldVote);
-    }
-
-    AppDataSource.transaction(async (entityManager) => {
-      await entityManager.insert(PostVote, {
-        userId,
-        postId,
-        value: realValue
-      })
+    return await AppDataSource.transaction(async (entityManager) => {
+      if (alreadyVoted) {
+        await entityManager.delete(PostVote, {
+          userId,
+          postId,
+        })
+      } else {
+        await entityManager.insert(PostVote, {
+          userId,
+          postId,
+          value: realValue
+        })
+      }
       const post = await entityManager.findOne(Post, {
         where: { id: postId },
         relations: { author: true }
@@ -91,12 +71,16 @@ export class VoteResolver {
       post.author.save();
       post.save();
 
+      if (alreadyVoted) return { voted: false, message: "removed vote" };
+
+      return {
+        voted: true,
+        message: realValue === 1 ? "upvoted" : "downvoted"
+      };
+    }).catch(() => {
+      return { voted: false, message: "database error" };
     })
 
-    return {
-      voted: true,
-      message: realValue === 1 ? "upvoted" : "downvoted"
-    };
   }
 
   @Mutation(() => VoteResponse, { nullable: true })
@@ -117,23 +101,43 @@ export class VoteResolver {
     if (oldVote?.value === realValue) {
       return {
         voted: false,
-        message: "already voted"
+        message: "invalid vote"
       };
     }
-    if (alreadyVoted) {
-      await CommentVote.remove(oldVote);
-      return { voted: false, message: "removed vote" };
-    }
 
-    await CommentVote.insert({
-      userId,
-      commentId,
-      value: realValue
-    });
+    return await AppDataSource.transaction(async (entityManager) => {
+      if (alreadyVoted) {
+        await entityManager.delete(CommentVote, {
+          userId,
+          commentId,
+        })
+      } else {
+        await entityManager.insert(CommentVote, {
+          userId,
+          commentId,
+          value: realValue
+        })
+      }
 
-    return {
-      voted: true,
-      message: realValue === 1 ? "upvoted" : "downvoted"
-    };
+      const comment = await entityManager.findOne(Comment, {
+        where: { id: commentId },
+        relations: { author: true }
+      })
+
+      comment.author.rating += realValue;
+      comment.rating += realValue;
+      await comment.author.save();
+      await comment.save();
+
+      if (alreadyVoted) return { voted: false, message: "removed vote" };
+
+      return {
+        voted: true,
+        message: realValue === 1 ? "upvoted" : "downvoted"
+      };
+    }).catch(() => {
+      return { voted: false, message: "database error" };
+    })
   }
 }
+
